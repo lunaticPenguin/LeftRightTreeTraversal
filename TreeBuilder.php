d8fdcde1872325cbf3114363865437f7df1b3a79
Corentin Legros <corentin.legros@gmail.com>
2013-07-31 11:58:36 +0200

    Adding basical tree class (without the body of compute method)

---
+++ TreeBuilder.php
@@ -0,0 +1,97 @@
+<?php
+
+class TreeBuilder {
+
+	protected $arrayNodes;
+
+	/**
+	 * Construct a new tree/graph
+	 */
+	public function __construct() {
+		$this->arrayNodes = array();
+	}
+
+	/**
+	 * Add node to graph tree
+	 * @param Node $objNode
+	 */
+	public function addNode(Node $objNode) {
+		$this->arrayNodes[$objNode->getId()] = $objNode;
+	}
+
+	/**
+	 * Set the node A parent of node B.
+	 * This method only delegates instructions to the Node class's methods.
+	 *
+	 * @param Node $objNodeA
+	 * @param Node $objNodeB
+	 */
+	public function setParentByNodes(Node $objNodeA, Node $objNodeB) {
+		$objNodeB->setParentNode($objNodeA);
+	}
+
+	/**
+	 * Set the node A child of node B.
+	 * This method only delegates instructions to the Node class's methods.
+	 *
+	 * @param Node $objNodeA
+	 * @param Node $objNodeB
+	 */
+	public function setChildByNodes(Node $objNodeA, Node $objNodeB) {
+		$objNodeB->addChild($objNodeA);
+	}
+
+	/**
+	 * Set the node A child of node B.
+	 * This method only delegates instructions to the Node class's methods.
+	 *
+	 * @param int $intNodeIdA id of node A
+	 * @param int $intNodeIdB id of node B
+	 */
+	public function setChildById($intNodeIdA, $intNodeIdB) {
+		if (isset($this->arrayNodes[$intNodeIdA]) && isset($this->arrayNodes[$intNodeIdA])) {
+			$this->arrayNodes[$intNodeIdB]->addChild($this->arrayNodes[$intNodeIdA]);
+		}
+	}
+
+	/**
+	 * Set the node A parent of node B.
+	 * This method only delegates instructions to the Node class's methods.
+	 *
+	 * @param int $intNodeIdA id of node A
+	 * @param int $intNodeIdB id of node B
+	 */
+	public function setParentById($intNodeIdA, $intNodeIdB) {
+		if (isset($this->arrayNodes[$intNodeIdA]) && isset($this->arrayNodes[$intNodeIdA])) {
+			$this->arrayNodes[$intNodeIdB]->setParentNode($this->arrayNodes[$intNodeIdA]);
+		}
+	}
+
+	/**
+	 * Compute the left and right value of for each node which belongs
+	 * to internal graph
+	 */
+	protected function compute() {
+
+	}
+
+	/**
+	 * Export internal graph to an array with adequate values
+	 * (it depends of the execution context)
+	 *
+	 * @return array FORMAT : array(array('id'=> #INTEGER, 'left' => #INTEGER, 'right' => #INTEGER))
+	 */
+	public function export() {
+
+		$this->compute();
+		$arrayResult = array();
+		foreach ($this->arrayNodes as $objNode) {
+			$arrayResult[] = array(
+				'id'	=> $objNode->getId(),
+				'left'	=> $objNode->getLeftValue(),
+				'right'	=> $objNode->getRightValue()
+			);
+		}
+		return $arrayResult;
+	}
+}
